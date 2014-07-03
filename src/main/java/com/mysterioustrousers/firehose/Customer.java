cc97ce3c899c46c11313b9c59eb50e614b87a34a
Dan Willoughby <amozoss@gmail.com>
2014-07-03 09:14:25 -0600

    adding interactions

---
+++ src/main/java/com/mysterioustrousers/firehose/Customer.java
@@ -0,0 +1,9 @@
+package com.mysterioustrousers.firehose;
+
+
+
+/**
+ * Created by dan on 7/2/14.
+ */
+public class Customer {
+}
--- src/main/java/com/mysterioustrousers/firehose/interactions/Interaction.java
+++ src/main/java/com/mysterioustrousers/firehose/interactions/Interaction.java
@@ -2,5 +2,50 @@ package com.mysterioustrousers.firehose.interactions;
 
 
 
-public class Interaction {
+import java.net.URL;
+import java.util.Date;
+
+import com.mysterioustrousers.firehose.Agent;
+import com.mysterioustrousers.firehose.Customer;
+import com.mysterioustrousers.firehose.FHObject;
+import com.mysterioustrousers.firehose.accounts.CustomerAccount;
+
+
+
+public class Interaction extends FHObject{
+
+  public enum InteractionChannel {
+    InteractionChannelEmail,
+    InteractionChannelTwitter,
+    InteractionChannelFacebook,
+    InteractionChannelChat
+};
+
+public enum InteractionHappiness {
+    InteractionHappinessUnknown,
+    InteractionHappinessUpset,
+    InteractionHappinessSatisfied,
+    InteractionHappinessHappy
+};
+
+
+  public Customer customer;
+
+  public String               token;
+  public String               responseDraft;
+  public InteractionHappiness happiness;
+  public boolean              resolved;
+  public boolean            isOutgoing;
+  public String             subject;
+  public String             body;
+  public URL                privateURL;
+  public InteractionChannel channel;
+  public Date               receivedAt;
+  public CustomerAccount    customerAccount;
+  public Agent              agent;
+
+  //public Set responseInteractions;
+  //public Set notes;
+  //public Set tags;
+  //public Set flaggedAgents;
 }
