1185b09e6cdc2f6aeeb05d500882d07dc395acf8
Dan Willoughby <amozoss@gmail.com>
2014-07-15 14:16:44 -0600

    update chatInteractions with getter and setters

--- src/main/java/com/mysterioustrousers/firehose/interactions/ChatInteraction.java
+++ src/main/java/com/mysterioustrousers/firehose/interactions/ChatInteraction.java
@@ -4,6 +4,8 @@ package com.mysterioustrousers.firehose.interactions;
 
 import java.util.Date;
 
+import com.google.gson.annotations.SerializedName;
+
 import com.mysterioustrousers.firehose.Agent;
 import com.mysterioustrousers.firehose.FHObject;
 import com.mysterioustrousers.firehose.Visitor;
@@ -18,19 +20,31 @@ public class ChatInteraction extends Interaction {
   }
 
 
-  public Visitor visitor;
+  private Visitor _visitor;
 
-  public String deliveredAt;
 
-  public String readAt;
 
-  public String editedAt;
+  @SerializedName("message_id")
+  private String _messageId;
 
-  public String failedAt;
+  @SerializedName("product_token")
+  private String _productToken;
 
-  public String senderDisplayName;
+  @SerializedName("delivered_at")
+  private String _deliveredAt;
 
-  public ChatInteractionKind kind;
+  @SerializedName("read_at")
+  private String _readAt;
+
+  @SerializedName("edited_at")
+  private String _editedAt;
+
+  @SerializedName("failed_at")
+  private String _failedAt;
+
+  private String _senderDisplayName;
+
+  private ChatInteractionKind _kind;
 
 
 
@@ -43,18 +57,17 @@ public class ChatInteraction extends Interaction {
 
 
   public static ChatInteraction chatInteractionWithBody(String body, Agent agent, Visitor visitor) {
-    // TODO some way of caching
     //ChatInteraction interaction  = ChatInteraction.chatInteractionWithIdentifier(this.generatedUUID()));
     ChatInteraction interaction = new ChatInteraction();
     interaction.setId(FHObject.generatedUUID());
     interaction.agent = agent;
     interaction.body = body;
-    interaction.visitor = visitor;
+    interaction.setVisitor(visitor);
     interaction.setCreatedAt(new Date().toString());
     interaction.isOutgoing = true;
     if (agent != null && agent.getFirstName() != null) {
       String firstName = agent.getFirstName();
-      interaction.senderDisplayName = !firstName.isEmpty() ? agent.getFirstName() : "Unknown";
+      interaction.setSenderDisplayName(!firstName.isEmpty() ? agent.getFirstName() : "Unknown");
     }
     return interaction;
   }
@@ -65,4 +78,111 @@ public class ChatInteraction extends Interaction {
     return (ChatInteraction)this.objectWithIdentifier:identifier];
   }
   */
+
+
+  // region Getter & Setters
+
+
+  @Override
+  public void setId(Object messageId) {
+    this._messageId = messageId.toString();
+  }
+
+
+  @Override
+  public Object getId() {
+    return _messageId;
+  }
+
+
+  public String getMessageId() {
+    return _messageId;
+  }
+
+
+  public void setMessageId(String _messageId) {
+    this._messageId = _messageId;
+  }
+
+
+  public ChatInteractionKind getKind() {
+    return _kind;
+  }
+
+
+  public void setKind(ChatInteractionKind kind) {
+    this._kind = kind;
+  }
+
+
+  public Visitor getVisitor() {
+    return _visitor;
+  }
+
+
+  public void setVisitor(Visitor visitor) {
+    this._visitor = visitor;
+  }
+
+
+  public String getProductToken() {
+    return _productToken;
+  }
+
+
+  public void setProductToken(String productToken) {
+    this._productToken = productToken;
+  }
+
+
+  public String getDeliveredAt() {
+    return _deliveredAt;
+  }
+
+
+  public void setDeliveredAt(String deliveredAt) {
+    this._deliveredAt = deliveredAt;
+  }
+
+
+  public String getReadAt() {
+    return _readAt;
+  }
+
+
+  public void setReadAt(String readAt) {
+    this._readAt = readAt;
+  }
+
+
+  public String getEditedAt() {
+    return _editedAt;
+  }
+
+
+  public void setEditedAt(String editedAt) {
+    this._editedAt = editedAt;
+  }
+
+
+  public String getFailedAt() {
+    return _failedAt;
+  }
+
+
+  public void setFailedAt(String failedAt) {
+    this._failedAt = failedAt;
+  }
+
+
+  public String getSenderDisplayName() {
+    return _senderDisplayName;
+  }
+
+
+  public void setSenderDisplayName(String senderDisplayName) {
+    this._senderDisplayName = senderDisplayName;
+  }
+
+  // endregion
 }
